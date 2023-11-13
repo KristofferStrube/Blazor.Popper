943e8b4a3e53b451639b592ba51ff58280591f5a
Mavaddat Javid <5055400+mavaddat@users.noreply.github.com>
2023-11-13 11:59:47 -0500

    Documentation

--- src/KristofferStrube.Blazor.Popper/Popper.cs
+++ src/KristofferStrube.Blazor.Popper/Popper.cs
@@ -6,26 +6,61 @@ using System.Threading.Tasks;
 
 namespace KristofferStrube.Blazor.Popper;
 
+/// <summary>
+/// A native C# wrapper for the Popper.js library
+/// </summary>
 public class Popper
 {
+    /// <summary>
+    /// The Popper JavaScript runtime
+    /// </summary>
     private readonly IJSRuntime jSRuntime;
+    /// <summary>
+    /// The Popper.js wrapper path
+    /// </summary>
     private const string PopperWrapperContent = "./_content/KristofferStrube.Blazor.Popper/KristofferStrube.Blazor.popper.js";
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Popper"/> class.
+    /// </summary>
+    /// <param name="jSRuntime">The Popper JavaScript runtime.</param>
     public Popper(IJSRuntime jSRuntime)
     {
         this.jSRuntime = jSRuntime;
     }
 
+    /// <summary>
+    /// Creates the Popper asynchronously.
+    /// </summary>
+    /// <param name="reference">The reference element (the container or context in which the Popper effect is shown).</param>
+    /// <param name="popper">The Popper effect to show.</param>
+    /// <param name="options">The Popper options.</param>
+    /// <returns></returns>
     public async Task<Instance> CreatePopperAsync(ElementReference reference, ElementReference popper, Options options)
     {
         return await CreatePopperAsync<ElementReference>(reference, popper, options);
     }
 
 
+    /// <summary>
+    /// Creates the Popper effect asynchronously.
+    /// </summary>
+    /// <param name="reference">The reference element (the container or context in which the Popper effect is shown).</param>
+    /// <param name="popper">The Popper effect to show.</param>
+    /// <param name="options">The Popper options.</param>
+    /// <returns></returns>
     public async Task<Instance> CreatePopperAsync(VirtualElement reference, ElementReference popper, Options options)
     {
         return await CreatePopperAsync<VirtualElement>(reference, popper, options);
     }
 
+    /// <summary>
+    /// Creates the Popper effect asynchronously.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="reference">The reference element (the container or context in which the Popper effect is shown).</param>
+    /// <param name="popper">The Popper effect to show.</param>
+    /// <param name="options">The Popper options.</param>
+    /// <returns></returns>
     private async Task<Instance> CreatePopperAsync<T>(T reference, ElementReference popper, Options options)
     {
         IJSObjectReference popperWrapper;
