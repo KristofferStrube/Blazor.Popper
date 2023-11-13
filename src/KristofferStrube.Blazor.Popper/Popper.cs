2dde93cc0e65399f4dae189ce916907d2f71ec00
Mavaddat Javid <5055400+mavaddat@users.noreply.github.com>
2023-11-13 10:44:11 -0500

    Refactor for Blazor generality fixes #4

--- src/KristofferStrube.Blazor.Popper/Popper.cs
+++ src/KristofferStrube.Blazor.Popper/Popper.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Options;
 using Microsoft.JSInterop;
+using System;
 using System.Threading.Tasks;
 
 namespace KristofferStrube.Blazor.Popper;
@@ -7,7 +9,7 @@ namespace KristofferStrube.Blazor.Popper;
 public class Popper
 {
     private readonly IJSRuntime jSRuntime;
-
+    private const string PopperWrapperContent = "./_content/KristofferStrube.Blazor.Popper/KristofferStrube.Blazor.popper.js";
     public Popper(IJSRuntime jSRuntime)
     {
         this.jSRuntime = jSRuntime;
@@ -15,15 +17,27 @@ public class Popper
 
     public async Task<Instance> CreatePopperAsync(ElementReference reference, ElementReference popper, Options options)
     {
-        var popperWrapper = await jSRuntime.InvokeAsync<IJSInProcessObjectReference>("import", "./_content/KristofferStrube.Blazor.Popper/KristofferStrube.Blazor.popper.js");
-        var jSInstance = await popperWrapper.InvokeAsync<IJSObjectReference>("createPopper", reference, popper, options);
-        return new(jSInstance, popperWrapper);
+        return await CreatePopperAsync<ElementReference>(reference, popper, options);
     }
+
 
     public async Task<Instance> CreatePopperAsync(VirtualElement reference, ElementReference popper, Options options)
     {
-        var popperWrapper = await jSRuntime.InvokeAsync<IJSInProcessObjectReference>("import", "./_content/KristofferStrube.Blazor.Popper/KristofferStrube.Blazor.popper.js");
+        return await CreatePopperAsync<VirtualElement>(reference, popper, options);
+    }
+
+    private async Task<Instance> CreatePopperAsync<T>(T reference, ElementReference popper, Options options)
+    {
+        IJSObjectReference popperWrapper;
+        try
+        {
+            popperWrapper = await jSRuntime.InvokeAsync<IJSInProcessObjectReference>("import", PopperWrapperContent);
+        }
+        catch (JSException)
+        {
+            popperWrapper = await jSRuntime.InvokeAsync<IJSObjectReference>("import", PopperWrapperContent);
+        }
         var jSInstance = await popperWrapper.InvokeAsync<IJSObjectReference>("createPopper", reference, popper, options);
-        return new(jSInstance, popperWrapper);
+        return new Instance(jSInstance, popperWrapper);
     }
 }
