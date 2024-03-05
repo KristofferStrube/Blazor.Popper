a30fbca1606515cb1663e431f1f652968d409099
KristofferStrube <KristofferStrube@users.noreply.github.com>
2024-03-05 22:25:22 +0000

    deploy: 493bba090969f0f218289b3e4f5843fa91a5a976

---
+++ _content/KristofferStrube.Blazor.Popper/KristofferStrube.Blazor.popper.js
@@ -0,0 +1,76 @@
+﻿export async function createPopper(reference, popper, options) {
+    cleanOptions(options);
+
+    if (!(reference instanceof Element)) {
+        var objRef = reference.objRef;
+        reference.getBoundingClientRect = () => objRef.invokeMethod('CallGetBoundingClientRect');
+        delete reference.objRef;
+    }
+
+    return Popper.createPopper(reference, popper, options);
+}
+
+export async function createPopperAsync(reference, popper, options) {
+    cleanOptions(options);
+
+    if (!(reference instanceof Element)) {
+        var objRef = reference.objRef;
+        var intermediateClientRectCache = {
+            "rect": {}
+        };
+        reference.getBoundingClientRect = () => {
+            objRef.invokeMethodAsync('CallGetBoundingClientRect').then(rect => intermediateClientRectCache.rect = rect);
+            return intermediateClientRectCache.rect;
+        };
+        delete reference.objRef;
+    }
+
+    return Popper.createPopper(reference, popper, options);
+}
+
+
+export function getStateOfInstance(instance) {
+    return stripState(instance.state)
+}
+
+export function updateOnInstance(instance) {
+    return instance.update().then(state => stripState(state));
+}
+
+export async function setOptionsOnInstance(instance, options) {
+    await cleanOptions(options);
+
+    return instance.setOptions(options).then(state => stripState(state));
+}
+
+function stripState(state) {
+    return {
+        attributes: state.attributes,
+        elements: Object.keys(state.elements),
+        modifiersData: state.modifiersData,
+        placement: state.placement,
+        // We need a custom serilizer/deserilizer before we can parse this back.
+        // orderedModifiers: state.orderedModifiers.map(modifier => ({ name: modifier.name, enabled: modifier.enabled, phase: modifier.phase })),
+    }
+}
+
+function cleanOptions(options) {
+    options.onFirstUpdate = (state) => options.objRef.invokeMethodAsync('CallOnFirstUpdate', stripState(state));
+
+    if (options.modifiers != null) {
+        options.modifiers = options.modifiers.map(modifier => {
+            var objRef = modifier.objRef;
+            if (modifier.hasFn) {
+                modifier.fn = (modifierArguments) => {
+                    modifierArguments.state = stripState(modifierArguments.state);
+                    delete modifierArguments.instance;
+                    delete modifierArguments.options;
+                    objRef.invokeMethodAsync('CallFn', modifierArguments);
+                }
+            }
+            delete modifier.objRef;
+            delete modifier.hasFn;
+            return modifier;
+        });
+    }
+}
